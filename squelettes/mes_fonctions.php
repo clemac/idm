09ff59da27df217e7f0e0e4e181ab874ba18759e
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-10-10 15:51:33 +0200

    Use CSS rather than PHP to make lettrines

--- squelettes/mes_fonctions.php
+++ squelettes/mes_fonctions.php
@@ -3,18 +3,6 @@
 global $tables_principales;
 $tables_principales['spip_auteurs']['field']['billettiste'] = "enum('oui','non') NOT NULL DEFAULT 'non'";
 
-function lettrine ($texte) {
-  $lines = explode ("\n", $texte);
-  for ($i=0; $i<count($lines); $i++) {
-    if (preg_match ('/(.*)<p>[\\s]*(<a[^>]*>)?((&#171;|[^<])(’)?)([^\\s]*)([\\s])/', $lines[$i], $matches)) {
-      $lettrine = $matches[1] . '<p class="spip lettrine">' . $matches[2] . '<span class="lettrine"><span class="lettrine_first">' . $matches[3] . '</span>' . $matches[6] . '</span>' . $matches[7];
-      $lines[$i] = str_replace ($matches[0], $lettrine, $lines[$i]);
-      break;
-    }
-  }
-  return implode ("\n", $lines);
-}
-
 function initiale ($mot) {
   return strtoupper($mot[0]);
 }
