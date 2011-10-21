35bbbf148dbe350b16ec5edd95902c44e6782c31
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-10-22 00:28:01 +0200

    Use proper language files

---
+++ plugins/IdM/lang/idm_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+                                       'billettistes' => "Liste des billettistes",
+
+                                       'menu'         => "IdM",
+                                       'moderation'   => "Interface de modération",
+
+                                       'projets'      => "Gestion des projets",
+
+                                       'relecteurs'   => "Liste des relecteurs",
+                                       'relecture'    => "Articles en cours de relecture",
+);
+
+?>
--- plugins/IdM/lang/paquet-idm_fr.php
+++ plugins/IdM/lang/paquet-idm_fr.php
@@ -1,7 +1,8 @@
 <?php
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-                                       'idm_slogan' => "Fonctions pour le fonctionnement d'Images des Maths",
+                                       'idm_nom'         => "IdM",
+                                       'idm_slogan'      => "Fonctions pour le fonctionnement d'Images des Maths",
                                        'idm_description' => " ",
 );
 
