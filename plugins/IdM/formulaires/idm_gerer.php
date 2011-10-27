818fe213b6c64b9a9c29b04aff5894779947271e
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-10-27 18:46:36 +0200

    Add the logic to actually use the planning tool

---
+++ plugins/IdM/formulaires/idm_gerer.php
@@ -0,0 +1,42 @@
+<?php
+
+function formulaires_idm_gerer_charger ($id_article) {
+  if ($GLOBALS['auteur_session']['statut'] != "0minirezo") return false;
+
+  $article = sql_fetsel ("*", "spip_articles", "id_article=$id_article");
+
+  $editeurs = array();
+  foreach (sql_allfetsel ("*", "spip_auteurs", "statut = '0minirezo'") as $e) {
+    $editeurs [$e["id_auteur"]] = idm_prenom_nom($e["nom"]);
+  }
+
+  $params = array(
+                  "id_article" => $id_article,
+                  "id_editeur" => $article["id_editeur"],
+                  "date_prevue" => $article["date_prevue"],
+                  "commentaires" => $article["commentaires"],
+                  "editeurs" => $editeurs,
+                  );
+
+  return $params;
+}
+
+function formulaires_idm_gerer_verifier ($id_article) {}
+
+function formulaires_idm_gerer_traiter ($id_article) {
+  $date = date_parse_from_format ("d/m/Y", _request("date_prevue"));
+  $mysqldate = "" . $date["year"] . "-" . $date["month"] . "-" . $date["day"];
+
+  $modif = array(
+                 "id_editeur" => intval(_request("id_editeur")),
+                 "date_prevue" => $mysqldate,
+                 "commentaires" => _request("commentaires"),
+                 );
+
+  sql_updateq ("spip_articles", $modif, "id_article = $id_article");
+
+  return array("message_ok" => "Modification effectu&eacute;e.",
+               "redirect" => generer_url_ecrire("idm_tableau"));
+}
+
+?>
