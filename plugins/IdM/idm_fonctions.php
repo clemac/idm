d9912fa6216fe95bedf3bf09fe80b492dbd0d727
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-12-07 12:27:30 +0100

    Do not include TeX/noErrors.js, it makes debugging harder

--- plugins/IdM/idm_fonctions.php
+++ plugins/IdM/idm_fonctions.php
@@ -257,7 +257,7 @@ function idm_insert_head ($texte) {
   $mj_insert = <<<END
     <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js">
       MathJax.Hub.Config({
-        extensions: ["tex2jax.js", "jsMath2jax.js", "TeX/noErrors.js", "TeX/AMSmath.js", "TeX/AMSsymbols.js"],
+        extensions: ["tex2jax.js", "jsMath2jax.js", "TeX/AMSmath.js", "TeX/AMSsymbols.js"],
         jax:        ["input/TeX",  "output/HTML-CSS"],
         tex2jax: {
           inlineMath:          [ ['$','$'],   ["\\\\(","\\\\)"] ],
