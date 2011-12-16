3d9790c41a9c2ad615d9f5d3d04bf1593f75c913
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-12-16 14:09:28 +0100

    [PLUGINS] More plugins can be automatically installed

--- plugins/cextra_core/lang/paquet-cextras_fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-
-	'cextras_titre' => 'Champs Extras',
-	'cextras_slogan' => 'Créer de nouveaux champs aux objets d\'édition de SPIP',
-	'cextras_description' => ' ',
-);
-?>
