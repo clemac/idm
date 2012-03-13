2d8436f08a51c0637eaaa77cf2bb1c359cfdb7b4
Vincent Beffara <vbeffara@ens-lyon.fr>
2012-03-13 18:32:27 +0200

    Production version should hide PHP warnings and errors

--- config/mes_options.php
+++ config/mes_options.php
@@ -4,7 +4,7 @@ define ('_ID_WEBMESTRES', '1');
 #define ('_SPIP_LOCK_MODE', 0);
 #define ('_LOG_FILTRE_GRAVITE', 7);
 
-ini_set('display_errors', '1');
+#ini_set('display_errors', '1');
 
 $type_urls = "propres2";
 
